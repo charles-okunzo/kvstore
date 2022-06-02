520ae738fde59f27fb29533349146ad81e6cf86d
charles okunzo <okunzopeter01@gmail.com>
2022-06-02 11:24:09 +0300

    feature --add project files

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut arguments = std::env::args().skip(1);
+    let key = arguments.next().unwrap();
+    let value = arguments.next().unwrap();
+    println!("key is {} and value is {}", key, value);
+}
